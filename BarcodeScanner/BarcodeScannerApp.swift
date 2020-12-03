b480b0fd1c6b72538d83e4028b97125185c4a692
Daniel Kilders <dankil.dev@gmail.com>
2020-12-03 22:46:22 +0100

    Added basic UI

--- BarcodeScanner/BarcodeScannerApp.swift
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -1,17 +1,10 @@
-//
-//  BarcodeScannerApp.swift
-//  BarcodeScanner
-//
-//  Created by Dani Kilders on 03.12.20.
-//
-
 import SwiftUI
 
 @main
 struct BarcodeScannerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BarcodeScannerView()
         }
     }
 }
